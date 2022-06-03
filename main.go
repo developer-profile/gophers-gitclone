9504cd9523943d707a3f7a3d88bd7f484f82812f
Dimitri Molokov <d@molokov.de>
2022-06-03 18:01:32 +0300

    main.go reading files

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {
+
+	// open file for reading
+	// read line by line
+	lines, err := readLines("gophers-gitclone.txt")
+	if err != nil {
+
+		log.Fatalf("readLines: %s", err)
+	}
+	// print file contents
+	for i, line := range lines {
+		fmt.Println(i, line)
+	}
+
+}
